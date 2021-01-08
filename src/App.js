74705fef2a37cb87f48d29ea6660d4cdc5ac2f72
Kn0wn-Un <naikdarsh175@gmail.com>
2021-01-08 12:12:42 +0530

    getting api data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Container, Button, IconButton, Box } from '@material-ui/core';
+import { Alarm } from '@material-ui/icons';
+import { useEffect, useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState({});
+    const [gotData, setGotData] = useState(false);
+    async function fetchResults() {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a60446147f601604724971a987162ebb`
+        );
+        if (!response.ok) {
+            setData(false);
+            return;
+        }
+        const res = await response.json();
+        setData(res);
+        setGotData(true);
+    }
+    useEffect(() => {
+        fetchResults();
+        console.log(data);
+    }, []);
+    return (
+        <Container maxWidth={false}>
+            <h1>
+                <code>{gotData ? data.name : 'Loading...'}</code>
+            </h1>
+            <IconButton color="secondary" aria-label="add an alarm">
+                <Alarm />
+            </IconButton>
+            <Button
+                variant="contained"
+                color="primary"
+                href="https://reactjs.org"
+                startIcon={<Alarm />}
+            >
+                Learn React
+            </Button>
+            <Box bgcolor="primary">Learn React</Box>
+        </Container>
+    );
 }
 
 export default App;
